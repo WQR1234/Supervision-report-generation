a463a20a1c8324ffbe85554816fa556d1cb6592f
WQR1234 <731557025@qq.com>
2024-07-14 16:34:13 +0800

    update requirements

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return "Hello World!!!!!!"
+    if request.method == "GET":
+        print(request.args)
+    elif request.method == "POST":
+        data = request.json
+        if 'abc' in data:
+            data['abc'] += 100
+        return jsonify(data)
+    return jsonify(request.args)
 
 
 if __name__ == "__main__":
